24a0c6c84c22e5701848b85dfc7b7f5630eb3823
Vladislav Yarmak <vladislav@vm-0.com>
2019-07-01 19:35:24 +0300

    add decryption

--- drb_client/__main__.py
+++ drb_client/__main__.py
@@ -23,19 +23,6 @@ SERVER_ENDPOINT = 'https://drand.cloudflare.com/api/private'
 SERVER_PUBKEY = '6302462fa9da0b7c215d0826628ae86db04751c7583097a4902dd2ab827b7c5f21e3510d83ed58d3f4bf3e892349032eb3cd37d88215e601e43f32cbbe39917d5cc2272885f2bad0620217196d86d79da14135aebb8191276f32029f69e2727a5854b21a05642546ebc54df5e6e0d9351ea32efae3cd9f469a0359078d99197c'
 TIMEOUT=5
 
-hash_len = 32
-def hmac_sha256(key, data):
-    return hmac.new(key, data, hashlib.sha256).digest()
-
-def hkdf(length, ikm, salt=b"", info=b""):
-    prk = hmac_sha256(salt if len(salt) > 0 else bytes([0]*hash_len), ikm)
-    t = b""
-    okm = b""
-    for i in range(ceil(length / hash_len)):
-        t = hmac_sha256(prk, t + info + bytes([1+i]))
-        okm += t
-    return okm[:length]
-
 def unmarshall_pubkey(pubkey):
     assert len(pubkey) == 4 * COORD_SIZE
     coords = tuple( int.from_bytes(pubkey[n*COORD_SIZE:(n+1) * COORD_SIZE], 'big') for n in range(4) )
@@ -46,7 +33,6 @@ def unmarshall_pubkey(pubkey):
     assert pk.is_on_curve()
     return pk
 
-
 def marshall_pubkey(pubkey):
     pubkey.force_affine()
     P = bn256.g2_marshall(pubkey)
@@ -56,10 +42,8 @@ def keygen():
     priv, pub = bn256.g2_random()
     return priv, pub
 
-def ecies_encrypt(recipient_pubkey, msg):
-    priv, pub = keygen()
-    dh_point = recipient_pubkey.scalar_mul(priv)
-    dh_bin = marshall_pubkey(dh_point)
+def key_from_point(point):
+    dh_bin = marshall_pubkey(point)
     backend = default_backend()
     hkdf = HKDF(algorithm=hashes.SHA256(),
                 length=32,
@@ -67,6 +51,12 @@ def ecies_encrypt(recipient_pubkey, msg):
                 info=None,
                 backend=backend)
     shared_key = hkdf.derive(dh_bin)
+    return shared_key
+
+def ecies_encrypt(recipient_pubkey, msg):
+    priv, pub = keygen()
+    dh_point = recipient_pubkey.scalar_mul(priv)
+    shared_key = key_from_point(dh_point)
     nonce = os.urandom(12)
     aesgcm = AESGCM(shared_key)
     ct = aesgcm.encrypt(nonce, msg, None)
@@ -79,34 +69,41 @@ def ecies_encrypt(recipient_pubkey, msg):
         'ciphertext': ct.hex(),
     }
 
-def make_req_body():
+def ecies_decrypt(privkey, box):
+    assert box['ephemeral']['gid'] == 22 # G2
+    eph_point = unmarshall_pubkey(bytes.fromhex(box['ephemeral']['point']))
+    dh_point = eph_point.scalar_mul(privkey)
+    shared_key = key_from_point(dh_point)
+    aesgcm = AESGCM(shared_key)
+    pt = aesgcm.decrypt(bytes.fromhex(box['nonce']),
+                        bytes.fromhex(box['ciphertext']),
+                        None)
+    return pt
+
+async def req_priv_rand():
+    timeout = aiohttp.ClientTimeout(total=TIMEOUT)
     priv, pub = keygen()
     server_pub = unmarshall_pubkey(bytes.fromhex(SERVER_PUBKEY))
     pub_bin = marshall_pubkey(pub)
     box = ecies_encrypt(server_pub, pub_bin)
+
     body = {
         "request": box,
     }
-    return body
-
-async def make_req():
-    timeout = aiohttp.ClientTimeout(total=TIMEOUT)
-    data = make_req_body()
     headers={
-        'content-type': 'application/json',
-        'connection': 'keep-alive',
         'user-agent': 'drb-client',
-        'accept': 'application/json',
     }
     async with aiohttp.ClientSession(timeout=timeout) as session:
         async with session.post(SERVER_ENDPOINT,
-                                json=data,
+                                json=body,
                                 headers=headers,
                                 allow_redirects=False) as resp:
-            return await resp.json()
+            res = await resp.json()
+    box = res['response']
+    return ecies_decrypt(priv, box)
 
 async def amain(loop):
-    res = await make_req()
+    res = await req_priv_rand()
     print(res)
 
 def main():
