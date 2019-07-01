8671380e6136674a0d6e039ed6ec56096f66fb3f
Vladislav Yarmak <vladislav@vm-0.com>
2019-07-01 13:51:30 +0300

    decoded pubkey

--- privtest.py
+++ privtest.py
@@ -7,13 +7,31 @@ import collections.abc
 
 import bn256
 
+COORD_SIZE = 32
+
 SERVER_ENDPOINT = 'https://drand.cloudflare.com:443/api/private'
 SERVER_PUBKEY = '6302462fa9da0b7c215d0826628ae86db04751c7583097a4902dd2ab827b7c5f21e3510d83ed58d3f4bf3e892349032eb3cd37d88215e601e43f32cbbe39917d5cc2272885f2bad0620217196d86d79da14135aebb8191276f32029f69e2727a5854b21a05642546ebc54df5e6e0d9351ea32efae3cd9f469a0359078d99197c'
 
+def unmarshall_pubkey(pubkey):
+    binary = bytes.fromhex(pubkey)
+    assert len(binary) == 4 * COORD_SIZE
+    coords = tuple( int.from_bytes(binary[n*COORD_SIZE:(n+1) * COORD_SIZE], 'big') for n in range(4) )
+    print(SERVER_PUBKEY)
+    for n in range(4):
+        print(binary[n*COORD_SIZE:(n+1) * COORD_SIZE].hex())
+    print(coords)
+    pk = bn256.curve_twist(
+        bn256.gfp_2(coords[0], coords[1]),
+        bn256.gfp_2(coords[2], coords[3]),
+        bn256.gfp_2(0,1))
+    assert pk.is_on_curve()
+    return pk
+
+
 def marshall_pubkey(pubkey):
     pubkey.force_affine()
+    print("pk=", pubkey)
     P = bn256.g2_marshall(pubkey)
-    print(P)
     return b''.join(coord.to_bytes() for coord in P).hex()
 
 def keygen():
@@ -22,6 +40,8 @@ def keygen():
 
 def make_req_body():
     priv, pub = keygen()
+    server_pub = unmarshall_pubkey(SERVER_PUBKEY)
+    pprint.pprint(server_pub)
     body = {
         "request": {
             "ephemeral": {
