4faa2f6b81ceb574bcaeab19a0fd317155042135
Vladislav Yarmak <vladislav@vm-0.com>
2019-07-02 13:54:36 +0300

    rename module

--- drb_client/__main__.py
+++ drb_client/__main__.py
@@ -11,7 +11,7 @@ from sdnotify import SystemdNotifier
 
 from .constants import LogLevel
 from . import utils
-from . import rest_client
+from . import net
 
 SERVER_ENDPOINT = 'https://drand.cloudflare.com/api/private'
 SERVER_PUBKEY = '6302462fa9da0b7c215d0826628ae86db04751c7583097a4902dd2ab827b7c5f21e3510d83ed58d3f4bf3e892349032eb3cd37d88215e601e43f32cbbe39917d5cc2272885f2bad0620217196d86d79da14135aebb8191276f32029f69e2727a5854b21a05642546ebc54df5e6e0d9351ea32efae3cd9f469a0359078d99197c'
@@ -67,7 +67,7 @@ async def amain(args, loop):  # pragma: no cover
     #                  pool=pool,
     #                  loop=loop)
     #await server.start()
-    res = await rest_client.req_priv_rand(SERVER_ENDPOINT, SERVER_PUBKEY)
+    res = await net.req_priv_rand(SERVER_ENDPOINT, SERVER_PUBKEY)
     print(res.hex())
     logger.info("Server started.")
 
